2bb141c1dcd61a19fdc395f53880d02438223913
Toni Da rodda <toni.da-rodda@epitech.eu>
2022-05-21 12:35:47 +0200

    Changes: fix name of somefiles

--- tests/env_test.go
+++ tests/env_test.go
@@ -6,25 +6,36 @@ import (
 	"testing"
 )
 
-func TestEnv(t *testing.T) {
+func TestSetEnvFile(t *testing.T) {
 	envi.SetEnvFile("../.env")
 	e := os.Getenv("ADRESS")
 	if e != "http://127.0.0.1:8200" {
-		t.Errorf("Incorect read of env")
+		t.Errorf("Unable to read [ADRESS] env variable")
 	}
 }
 
-func TestEnv2(t *testing.T) {
+func TestSetEnvFile2(t *testing.T) {
 	envi.SetEnvFile("../.env")
 	e := os.Getenv("PATHREAD")
-	if e != "secret/data/abd" {
-		t.Errorf("Incorect read of env")
+	if e != "secret/data/db" {
+		t.Errorf("Unable to read [PATHREAD] env variable")
 	}
 }
-func TestEnv3(t *testing.T) {
+func TestSetEnvFile3(t *testing.T) {
 	envi.SetEnvFile("../.env")
 	e := os.Getenv("TOKEN")
-	if e != "hvs.DPCpBiRQdMUMMoZdrzDvIceQ" {
-		t.Errorf("Incorect read of env")
+	if e != "hvs.C5rcwKI673pSTlwell5bRHCG" {
+		t.Errorf("Unable to read [TOKEN] env variable")
+	}
+}
+
+func TestGetEnvFile(t *testing.T) {
+	env := envi.GetEnvFile("../.env")
+
+	if env[0] != "ADRESS=http://127.0.0.1:8200" {
+		t.Errorf("Wron value")
+	}
+	if env[1] != "TOKEN=hvs.C5rcwKI673pSTlwell5bRHCG" {
+		t.Errorf("Wron value")
 	}
 }
